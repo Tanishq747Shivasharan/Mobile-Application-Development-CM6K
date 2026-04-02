90b807003f5021edd33ff090918d2b4c11fbc90c
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-04-02 22:49:05 +0530

    Initial Android Base Project

---
+++ app/src/main/java/com/example/examinationprep/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.examinationprep;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText etname;
+    TextView tvgreet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btn = findViewById(R.id.greet);
+        etname = findViewById(R.id.name);
+        tvgreet = findViewById(R.id.greetings);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etname.getText().toString();
+                tvgreet.setText("Hello " + name + "!");
+            }
+        });
+    }
+}
