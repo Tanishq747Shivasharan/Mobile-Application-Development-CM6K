79b5691e4dc56e5e7af60eef350091de0e4b0002
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-04-04 13:39:34 +0530

    Intents in Android Devlopment Explicit and Implicit Intents.

--- app/src/main/java/com/example/examinationprep/MainActivity.java
+++ app/src/main/java/com/example/examinationprep/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.examinationprep;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button expbtn, impbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        expbtn = findViewById(R.id.btnExplicit);
+        impbtn = findViewById(R.id.btnImplicit);
 
+        expbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+
+                i.putExtra("user_key", "Tanishq Shivasharan");
+
+                startActivity(i);
+            }
+        });
+
+        impbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://www.google.com"));
+                startActivity(i);
+            }
+        });
     }
 }
